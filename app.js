1e2a2588cb14b42c3a27cbcbceecba6d1cb1a923
RosarioManny <rosario.emm47@gmail.com>
2024-08-29 14:12:46 -0400

    Exercise 1-6

---
+++ app.js
@@ -0,0 +1,35 @@
+// Exercise 1
+
+const foods = [];
+
+console.log("Exercise 1:", foods);
+
+//Exercise 2
+
+foods.push("Pizza", "Cheeseburger");
+
+console.log("Exercise 2:", foods);
+
+//Exercise 3
+
+foods.unshift("Taco");
+
+console.log("Exercise 3:", foods);
+
+//Exercise 4
+
+let favFood = foods[1];
+
+console.log("Exercise 4:", favFood);
+
+// Exercise 5
+
+foods.splice(2, 0, "Tofu");
+
+console.log("Exercise 5:", foods);
+
+// Exercise 6
+
+foods.splice(1, 2, "sushi", "cupcake");
+
+console.log("Exercise 6:", foods);
